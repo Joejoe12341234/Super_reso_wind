149273757ccddf4477d2470602be92588aec9ca1
Joe Lockwood <86720316+Joejoe12341234@users.noreply.github.com>
2024-09-11 07:20:58 +0100

    Add files via upload

--- Optimization_tests.sh
+++ Optimization_tests.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Define arrays of parameters
-dropout_rates=(0 0.1)
-l2_regs=(0 0.05)
-weight_exps=(1 2 4)
-epochs=(400 600 1000 1500 3400 4000)
-learning_rates=(0.0001 0.00001)
+dropout_rates=(0 0.05 0.1 0.5 0.75)
+l2_regs=(0 0.05 0.1 0.5 0.75)
+weight_exps=(0 1 2 4 10)
+epochs=(250 500 750 1000 1500 3400 4000 10000 50000)
+learning_rates=(0.1 0.01 0.001 0.0001 0.00001 0.000001 0.000001)
 criteria=('extreme' 'mse' 'quantile')
-scalings=('Robust' 'MinMax')
-# results_dropout_0.1_l2_0.0_weight_2_criterion_quantile_lr_0.0001_epoch_3400_scaler_RobustScaler()
-
+scalings=('Robust' 'StandardScalar' 'MinMax')
+ 
 # Loop over each combination of parameters and submit a SLURM job
 for dropout_rate in "${dropout_rates[@]}"; do
   for l2_reg in "${l2_regs[@]}"; do
@@ -26,11 +25,10 @@ for dropout_rate in "${dropout_rates[@]}"; do
 #SBATCH --job-name=mnist-${dropout_rate}-${l2_reg}-${weight_exp}-${epoch}-${learning_rate}-${criterion}-${scaling}-$random_number
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=gpu80
 #SBATCH --time=10:00:00
-#SBATCH --mail-user=jl115@princeton.edu
+#SBATCH --mail-user= XXX
 
 module load anaconda3/2023.9 
 conda activate base
